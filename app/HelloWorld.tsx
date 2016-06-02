2b6997596ae40b96639053244634d601ff872b24
Konstantin Kolesov <Konstantin_Kolesov@epam.com>
2016-06-02 11:02:23 +0300

    start

---
+++ app/HelloWorld.tsx
@@ -0,0 +1,10 @@
+///<reference path="../typings/react/react-global.d.ts"/>
+
+class HelloWorld extends React.Component<any, any> {
+    render() {
+        return <div>Hello World!!! =)</div>
+    }
+}
+
+ReactDOM.render(<HelloWorld/>,
+    document.getElementById('app'));
